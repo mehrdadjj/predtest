59c6911e1a2220b46f5c0074b346c67826a2d619
aminbuilderking <amin@builderking.io>
2023-11-29 07:48:43 +0330

    fs

--- roop/face_analyser.py
+++ roop/face_analyser.py
@@ -47,8 +47,8 @@ def find_similar_face(frame: Frame, reference_face: Face) -> Optional[Face]:
     many_faces = get_many_faces(frame)
     if many_faces:
         for face in many_faces:
-            if hasattr(face, 'normed_embedding') and hasattr(reference_face, 'normed_embedding'):
-                distance = numpy.sum(numpy.square(face.normed_embedding - reference_face.normed_embedding))
-                if distance < roop.globals.similar_face_distance:
-                    return face
+            return face
+    # if hasattr(face, 'normed_embedding') and hasattr(reference_face, 'normed_embedding'):
+    #     distance = numpy.sum(numpy.square(face.normed_embedding - reference_face.normed_embedding))
+    #     if distance < roop.globals.similar_face_distance:
     return None
